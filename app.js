4f7a905a54d95af9a48145eeb9365d89f0430d8f
Ali Şahin <76913900+alisahinpisirici@users.noreply.github.com>
2022-07-14 17:31:11 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,30 +1,21 @@
-
 let scanner = new Instascan.Scanner({ video: document.getElementById('preview'),
-//Eldeki görüntüde sürekli QRCode taraması yapılıp yapılmamasını belirler.
-continuous: true,
-mirror: true,
-//Tarama neticesinde okunan QRCode verisinin görüntüsünü sonuca dahil
-//ederek ikinci paramere(image) iletir. Varsayılan false değerindedir.
-captureImage: true,
-backgroundScan: true,
-//Taranan bir QRCode'un bulunduğu görüntüde kaç saniye aralıkla
-//tekrar taranacağını belirtir. Burada 10 saniye olarak belirlenmiştir.
-refractoryPeriod: 10000,
-//Tarama görüntüsünde kaç karenin taranacağını belirler.
-scanPeriod: 15 });
+   continuous: true,
+   mirror: true,
+   captureImage: true,
+   backgroundScan: true,
+   refractoryPeriod: 10000,
+   scanPeriod: 15 
+});
 
 scanner.addListener('scan', function (content) {
-   //Barkod okunduğunda veriyi burası yakalayacaktır.
    alert(content)
 });
  
-let cameraList = []; //Tüm kameraları tutacağımız liste.
+let cameraList = []; 
  
 Instascan.Camera.getCameras().then(function (cameras, image) {
-   //PC'de ki tüm kameraları algılayarak bizlere getirecektir.
    this.cameraList = cameras;
    cameras.forEach(element => {
-      //Gelen kameralar select elementinde listelenmektedir.
       let cameraList = document.getElementById("cameras");
       let option = document.createElement("option");
       option.text = element.id;
@@ -35,6 +26,5 @@ Instascan.Camera.getCameras().then(function (cameras, image) {
 }).catch(() => console.error(e));
  
 document.getElementById("cameras").addEventListener("change", event => {
-   //Select elementinde seçilen kamerayı qr code scanner olarak belirleyen olaydır.
    scanner.start(this.cameraList.find(c => c.id == event.target.value));
 })
